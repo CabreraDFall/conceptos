7b22d755880ad790ce65a83caf80492bf31db49b
CabreraDFall <testingdfall@gmail.com>
2024-12-01 10:05:36 +0100

    Error: Handle

--- weatherApp/scripts.js
+++ weatherApp/scripts.js
@@ -1,22 +1,38 @@
 let weatherData;
 
-const API_KEY = "654b4ec814040f59fbe3fa9fe503e429";
+const API_KEY = "";
 const btnEvent = document.getElementById("form");
 const cityInput = document.getElementById("city");
 const weatherResult = document.getElementById("weather-result");
+const errorPrint = document.getElementById("error");
 
 btnEvent.addEventListener("submit", (e) => {
   e.preventDefault();
+  displayError("");
   const cityvalue = cityInput.value;
   callTheAPI(cityvalue, API_KEY);
 });
 
 const callTheAPI = (city, API_KEY) => {
+  displayError("cargando...");
+  weatherResult.innerHTML = "";
+
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
   )
-    .then((response) => response.json())
-    .then((weatherData) => displayWeather(weatherData));
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Error en la solicitud: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((weatherData) => {
+      displayError("");
+      displayWeather(weatherData);
+    })
+    .catch((error) => {
+      displayError(error.message);
+    });
 };
 
 const displayWeather = (data) => {
@@ -32,4 +48,8 @@ const displayWeather = (data) => {
 
 const kelvinToCelsius = (temp) => {
   return (temp - 273.15).toFixed(2);
+};
+
+const displayError = (error) => {
+  errorPrint.innerHTML = `${error}`;
 };
